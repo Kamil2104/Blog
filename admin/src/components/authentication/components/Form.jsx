911d15481fc53c6e30e5d36f66a007af5407a207
pindrive <kamilniewiadomski18@gmail.com>
2024-01-21 19:14:26 +0100

    feat: add file for displaying form for authentication

---
+++ admin/src/components/authentication/components/Form.jsx
@@ -0,0 +1,17 @@
+const Form = () => {
+  return (
+    <div className="form">
+        <div className="header">
+        
+        </div>
+        <div className="inputs">
+
+        </div>
+        <div className="button">
+
+        </div>
+    </div>
+  )
+}
+
+export default Form

2ee7764cca61ff9739bba6cfb9ba00d1b7d58a9d
pindrive <kamilniewiadomski18@gmail.com>
2024-01-21 19:28:00 +0100

    feat: add inputs and button for login form

--- admin/src/components/authentication/components/Form.jsx
+++ admin/src/components/authentication/components/Form.jsx
@@ -2,10 +2,21 @@ const Form = () => {
   return (
     <div className="form">
         <div className="inputs">
-
+            <input 
+                type="text"
+                spellCheck="false" 
+                autoComplete="true"
+            />
+            <input 
+                type="password" 
+                spellCheck="false"
+                autoComplete="false" 
+            />
         </div>
         <div className="button">
-
+            <button>
+                Log in
+            </button>
         </div>
     </div>
   )

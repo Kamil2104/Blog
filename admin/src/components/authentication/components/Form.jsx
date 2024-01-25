bafe2f1b4605f8feb88c6dc319a5ca96615f3d74
pindrive <kamilniewiadomski18@gmail.com>
2024-01-25 22:17:21 +0100

    feat: get information if user logged in correctly using axios

--- admin/src/components/authentication/components/Form.jsx
+++ admin/src/components/authentication/components/Form.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { faLock } from '@fortawesome/free-solid-svg-icons'
+
+import axios from 'axios'
 
 import '../assets/Form.css'
 
 const Form = () => {
     const [showPassword, setShowPassword] = useState(false)
 
+    const loginRef = useRef("")
+    const passwordRef = useRef("")
+
     const togglePasswordVisibility = () => {
         setShowPassword(prev => !prev)
+    }
+
+    const handleLogin = () => {
+        const values = {
+            login: loginRef.current.value,
+            password: passwordRef.current.value
+        }
+        
+        axios.post('http://localhost:3001/login', values)
+            .then(res => {
+                if(res.data === "Success") {
+                    // Navigate
+                } else if (res.data === "Fail") {
+                    // Incorrect login or password
+                } else {
+                    alert("Something went wrong with our servers. Try again later.")
+                }
+            })
+            .catch(err => console.log(err))
     }
 
   return (
@@ -27,13 +51,15 @@ const Form = () => {
                 spellCheck="false" 
                 autoComplete="true"
                 placeholder="Username:"
+                ref={loginRef}
             />
             <div className="inputPassword">
                 <input 
                     type={showPassword ? "text" : "password"}
                     spellCheck="false"
                     autoComplete="false"
-                    placeholder="Password:" 
+                    placeholder="Password:"
+                    ref={passwordRef} 
                 />
                 <FontAwesomeIcon
                     icon={showPassword ? faEyeSlash : faEye}
@@ -43,7 +69,7 @@ const Form = () => {
             </div>
         </div>
         <div className="button">
-            <button>
+            <button onClick={handleLogin}>
                 Log in
             </button>
         </div>

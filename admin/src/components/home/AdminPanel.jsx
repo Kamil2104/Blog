9769b8325ec07ff1b6b55c4b368caa5fa8bcf9e2
pindrive <kamilniewiadomski18@gmail.com>
2024-01-29 21:52:44 +0100

    feat: change name of exported main file function

--- admin/src/components/home/AdminPanel.jsx
+++ admin/src/components/home/AdminPanel.jsx
@@ -1,4 +1,4 @@
-const Main = () => {
+const AdminPanel = () => {
   return (
     <div> 
       <h1> AAA </h1>
@@ -6,4 +6,4 @@ const Main = () => {
   )
 }
 
-export default Main
+export default AdminPanel

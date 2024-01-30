41785c45e86d3690340508ceb90d5a2f6fa2dbaf
pindrive <kamilniewiadomski18@gmail.com>
2024-01-30 19:44:12 +0100

    feat: add functionality for logging out using database

--- admin/backend/server.js
+++ admin/backend/server.js
@@ -1,58 +1,25 @@
+/* eslint-disable no-undef */
+
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
 
+const router = express.Router();
 const PORT = 3001;
+
+const actions = require('./actions')
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-const blogdb = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "blogdb"
-})
-
-blogdb.connect((err) => {
-    if (err) {
-        console.log(err.message)
-    } 
-})
-
 app.listen(PORT, () => {
     console.log("Listening on port:",`${PORT}`)
 })
 
-app.post('/login', (req, res) => {
-    const loginQuery = "SELECT * FROM admin WHERE `login` = ?"
-    const passwordQuery = "SELECT * FROM admin WHERE `login` = ? AND `password` = ?"
+app.use('/', router);
 
-    const values = [
-        req.body.login,
-        req.body.password
-    ]
+// Actions
 
-    blogdb.query(loginQuery, values[0], (err, data) => {
-        if (err) {
-            return res.json("Error")
-        }
+router.post('/login', actions.authentication)
 
-        if (data.length > 0) {
-            blogdb.query(passwordQuery, values, (err, data) => {
-                if (err) {
-                    return res.json("Error")
-                }
-
-                if (data.length > 0) {
-                    return res.json("Success")
-                } else {
-                    return res.json("Invalid password")
-                }
-            })
-        } else {
-            return res.json("Invalid login")
-        }
-    });
-})
+router.post('/logOut', actions.logOut)
--- admin/src/components/home/components/Header.jsx
+++ admin/src/components/home/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom'
+
+import axios from 'axios'
+
 import '../assets/Header.css'
 
 // eslint-disable-next-line react/prop-types
 const Header = ({ setActualPanel }) => {
+    const navigate = useNavigate()
+
     const handleCreateBlog = () => {
         setActualPanel("Create")
     }
@@ -11,7 +17,15 @@ const Header = ({ setActualPanel }) => {
     }
 
     const handleLogOut = () => {
-
+        axios.post('http://localhost:3001/logOut')
+        .then(res => {
+            if (res.data === "Success") {
+                navigate('/')
+            } else {
+                alert("Something went wrong with our servers. Try again later.")
+            } 
+        })
+        .catch(err => console.log(err))
     }
 
   return (

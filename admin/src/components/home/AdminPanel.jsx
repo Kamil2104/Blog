65f18534c24062f4c7ef86073518de566b7b0bfd
pindrive <kamilniewiadomski18@gmail.com>
2024-01-30 19:45:21 +0100

    refactor: delete unuseful useEffect

--- admin/src/components/home/AdminPanel.jsx
+++ admin/src/components/home/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import Header from './components/Header'
 
@@ -6,10 +6,6 @@ import './assets/AdminPanel.css'
 
 const AdminPanel = () => {
   const [actualPanel, setActualPanel] = useState("Create")
-
-  useEffect(() => {
-    alert(actualPanel)
-  }, [actualPanel])
 
   return (
 

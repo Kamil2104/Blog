16bb21544000119cf519f82a60f1cf9fbf3f84eb
pindrive <kamilniewiadomski18@gmail.com>
2024-01-31 18:21:21 +0100

    refactor: resolve issue #15

--- admin/src/components/home/components/error/NotLoggedIn.jsx
+++ admin/src/components/home/components/error/NotLoggedIn.jsx
@@ -1,26 +1,24 @@
-import { useNavigate } from 'react-router-dom'
-
-import Loader from '../Loader'
-
-import './assets/NotLoggedIn.css'
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loader from '../Loader';
+import './assets/NotLoggedIn.css';
 
 const NotLoggedIn = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      navigate('/')
-    }, 3000)
+      navigate('/');
+    }, 3000);
 
-    return () => clearTimeout(timeout)
-  }, [navigate])
+    return () => clearTimeout(timeout);
+  }, [navigate]);
 
   return (
     <div className="notLoggedIn">
-        <Loader />
+      <Loader />
     </div>
-  )
-}
+  );
+};
 
-export default NotLoggedIn
+export default NotLoggedIn;

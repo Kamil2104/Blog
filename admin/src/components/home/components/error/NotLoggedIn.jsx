3582393dad645594dc3905ed447cd9d5307ec389
pindrive <kamilniewiadomski18@gmail.com>
2024-01-31 12:09:04 +0100

    refactor: change NotLoggedIn - display there for 3 seconds loader component and then automatically navigate to authentication page

--- admin/src/components/home/components/error/NotLoggedIn.jsx
+++ admin/src/components/home/components/error/NotLoggedIn.jsx
@@ -1,18 +1,24 @@
 import { useNavigate } from 'react-router-dom'
 
+import Loader from '../Loader'
+
 import './assets/NotLoggedIn.css'
+import { useEffect } from 'react'
 
 const NotLoggedIn = () => {
   const navigate = useNavigate()
-  
-  const handleNavigateToAuthentication = () => {
-    navigate('/')
-  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate('/')
+    }, 3000)
+
+    return () => clearTimeout(timeout)
+  }, [navigate])
 
   return (
     <div className="notLoggedIn">
-        <p> You are not logged in! </p>
-        <button onClick={handleNavigateToAuthentication}> Go to login page </button>
+        <Loader />
     </div>
   )
 }

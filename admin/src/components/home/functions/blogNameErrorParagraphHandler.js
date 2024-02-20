18725be1808afadc0226b0ac79de6d58424a978c
pindrive <kamilniewiadomski18@gmail.com>
2024-02-20 14:39:37 +0100

    feat: add scroll functionality when filled input value for blog name contains name which already exists

--- admin/src/components/home/functions/blogNameErrorParagraphHandler.js
+++ admin/src/components/home/functions/blogNameErrorParagraphHandler.js
@@ -1,9 +1,11 @@
 import { setRedBorder } from "./setRedBorderHandler";
+import { scrollToComponent } from '../functions/scrollToElementHandler'
 
 export function showBlogNameErrorParagraph(paragraphId) {
     let paragraph = document.getElementById(paragraphId)
 
     setRedBorder("blogName")
+    scrollToComponent("blogPhoto")
     
     setTimeout(() => {
         paragraph.style.visibility = "visible";

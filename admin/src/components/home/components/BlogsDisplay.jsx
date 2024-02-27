f9766cb7702e85969192e6d6777b8757ca62c437
pindrive <kamilniewiadomski18@gmail.com>
2024-02-27 20:57:23 +0100

    style: add responsive design for blog card display

--- admin/src/components/home/components/BlogsDisplay.jsx
+++ admin/src/components/home/components/BlogsDisplay.jsx
@@ -1,15 +1,35 @@
-import { useEffect } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrashCan } from '@fortawesome/free-regular-svg-icons'
 
-const BlogsDisplay = (blogData) => {
-  useEffect(() => {
-    if (blogData) {
-      // TODO: Add functionality for displaying blogs (blogData is an object)
-    }
-  }, [blogData])
+import '../assets/styles/BlogsDisplay.css'
+
+const BlogsDisplay = () => {
 
   return (
-    <div> 
+    <div className='blogsDisplay'> 
         {/* Display blogs here */}
+        <section className='blog'>
+          <section className='blogName'>
+            <p> To, co dzisiaj mnie spotkało zdziwi was wszystkich. Nawet ja się tego nie spodziewałem, a to rzadkie </p>
+          </section>
+          <section className='blogIcons'>
+            <FontAwesomeIcon 
+              icon={faEdit}
+              className='editIcon'
+            />
+
+            <FontAwesomeIcon 
+              icon={faTrashCan}
+              className='deleteIcon'
+            />
+          </section>
+        </section>
+        <section className='blog'>
+
+        </section>
+        <section className='blog'>
+
+        </section>
     </div>
   )
 }

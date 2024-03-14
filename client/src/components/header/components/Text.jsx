f35c80baab36c9dc01c6be3cf32da4cdc7881ad8
pindrive <kamilniewiadomski18@gmail.com>
2024-03-14 18:48:32 +0100

    feat: add content for Text component

--- client/src/components/header/components/Text.jsx
+++ client/src/components/header/components/Text.jsx
@@ -1,7 +1,14 @@
 const Text = () => {
   return (
     <>
-        Hello, my name is Kamil
+        <h1> Hello, I&apos;m Kamil! </h1>
+        <p> 
+            I&apos;m an 18-year-old student majoring in programming engineering. 
+            In school I&apos;ve been learning the basics of programming.
+            I set the goal to become a web developer one day, so I decided to take my basics to the next level - by myself.
+            Here I am - I&apos;ve created a blog web application using for example React, Express and Jest, which I&apos;ve been learning since November 27, 2023.
+            I hope you will enjoy this page :)   
+        </p>
     </>
   )
 }

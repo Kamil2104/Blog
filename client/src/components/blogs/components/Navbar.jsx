24ce1375d25094eedf398aeb49fbbaca07bdc194
pindrive <kamilniewiadomski18@gmail.com>
2024-03-18 18:43:54 +0100

    style: add style for navbar component

--- client/src/components/blogs/components/Navbar.jsx
+++ client/src/components/blogs/components/Navbar.jsx
@@ -1,7 +1,38 @@
+import { faHome, faMapPin } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const Navbar = () => {
   return (
     <>
-        <p> About me </p>
+        <section className="navigateToStart">
+            <p> Start </p>
+            <FontAwesomeIcon
+                icon={faHome}
+                className="pinnedBlogIcon"
+            />
+        </section>
+        <section className="navigateToPinnedBlog">
+            <p> About me </p>
+            <FontAwesomeIcon
+                icon={faMapPin}
+                className="pinnedBlogIcon"
+            />
+        </section>
+        <section className="navigateToDatabaseBlog">
+            <p> First Blog </p>
+        </section>
+        <section className="navigateToDatabaseBlog">
+            <p> Second Blog </p>
+        </section>
+        <section className="navigateToDatabaseBlog">
+            <p> Third Blog </p>
+        </section>
+        <section className="navigateToDatabaseBlog">
+            <p> Fourth Blog </p>
+        </section>
+        <section className="navigateToDatabaseBlog">
+            <p> Fifth Blog </p>
+        </section>
     </>
   )
 }

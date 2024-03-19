4f8a0c5e6eb136fc347f49f2de143002418e1a5b
pindrive <kamilniewiadomski18@gmail.com>
2024-03-19 10:56:03 +0100

    feat: create a loader

---
+++ client/src/components/loader/components/Loader.jsx
@@ -0,0 +1,13 @@
+import '../assets/Loader.css'
+
+const Loader = () => {
+  return (
+    <div className="loader">
+        <section className='loaderAnimation'>
+
+        </section>
+    </div>
+  )
+}
+
+export default Loader

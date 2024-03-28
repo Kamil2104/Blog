a0798d727af6530b2328d99c4736fc33395ddfb6
pindrive <kamilniewiadomski18@gmail.com>
2024-03-28 12:37:42 +0100

    refactor: format the code

--- client/src/components/blogs/components/Navbar.jsx
+++ client/src/components/blogs/components/Navbar.jsx
@@ -8,29 +8,29 @@ import PropTypes from "prop-types"
 const Navbar = ({ blogNames }) => {
 
     return (
-    <>
-        <section className="navigateToStart" onClick={() => scrollToComponent("header")}>
-            <p> Start </p>
-            <FontAwesomeIcon
-                icon={faHome}
-                className="pinnedBlogIcon"
-            />
-        </section>
-        <section className="navigateToPinnedBlog" onClick={() => scrollToComponent("pinnedBlogName")}>
-            <p> About me </p>
-            <FontAwesomeIcon
-                icon={faMapPin}
-                className="pinnedBlogIcon"
-            />
-        </section>
-        {blogNames.map((blog) => (
-            <section className="navigateToDatabaseBlog" key={blog.name}>
-                <section className={blog.name} onClick={() => scrollToComponent(blog.name)}>
-                    <p> {blog.name} </p>
-                </section>
+        <>
+            <section className="navigateToStart" onClick={() => scrollToComponent("header")}>
+                <p> Start </p>
+                <FontAwesomeIcon
+                    icon={faHome}
+                    className="pinnedBlogIcon"
+                />
             </section>
-        ))}
-    </>
+            <section className="navigateToPinnedBlog" onClick={() => scrollToComponent("pinnedBlogName")}>
+                <p> About me </p>
+                <FontAwesomeIcon
+                    icon={faMapPin}
+                    className="pinnedBlogIcon"
+                />
+            </section>
+            {blogNames.map((blog) => (
+                <section className="navigateToDatabaseBlog" key={blog.name}>
+                    <section className={blog.name} onClick={() => scrollToComponent(blog.name)}>
+                        <p> {blog.name} </p>
+                    </section>
+                </section>
+            ))}
+        </>
   )
 }
 

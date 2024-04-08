783acf712936eb7888426ab4aca46ca7365f2b6e
pindrive <kamilniewiadomski18@gmail.com>
2024-04-08 22:49:27 +0200

    feat: display formatted date frim database (using moment)

--- client/src/components/blogs/components/FullBlog.jsx
+++ client/src/components/blogs/components/FullBlog.jsx
@@ -1,11 +1,13 @@
 import { useLocation } from 'react-router-dom' 
 
+import Loader from '../../loader/components/Loader'
+
 import useFetchBlog from '../hooks/useFetchBlog'
 
 import PropTypes from "prop-types"
+import moment from 'moment'
 
 import '../assets/FullBlog.css'
-import Loader from '../../loader/components/Loader'
 
 const FullBlog = () => {
   const location = useLocation()
@@ -31,7 +33,7 @@ const FullBlog = () => {
               </aside>
           </main>
               <footer>
-                  date and time
+                  {moment(blog[0]?.date).format("YYYY-MM-DD, HH:mm")}
               </footer>
           </>
       )}
